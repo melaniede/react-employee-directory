6241074ebbba488736cbeed0b6ad83f005233c9b
melaniede <debarrosmelanie@gmail.com>
2020-11-03 20:37:15 -0500

    Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header () {
+    return (
+        <div class="navbar navbar-light bg-light text-center">
+        {/* <div class="App-header"> */}
+            <h1>Employee Directory</h1>
+        </div>
+    )
+}
+
+export default Header;
